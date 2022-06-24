b0d283cfab81443ab1481db391b4523caa5eae2e
fsda <kirillbazaroff@gmail.com>
2022-06-24 17:30:26 +0300

    Код самой игры + тейбл для дб + инсерт

--- RPS/SQl_tema.py
+++ RPS/SQl_tema.py
@@ -1,31 +1,32 @@
 from sqlalchemy import MetaData, Table, Column, String, Integer, create_engine, ForeignKey
+# import RPS.py
+
 
 meta = MetaData()
 
-authors = Table("lol&", meta,
-                Column('id_game', Integer, primary_key=True),
-                Column('name', String(255), nullable=False),
-                Column('comp_id',String(255)),
-                Column('rock', Integer),
-                Column('name', Integer),
-                Column('name', Integer)
-                )
+statistics = Table("lol&", meta,
+                   Column('id_game', Integer, primary_key=True),
+                   Column('name', String(255), nullable=False),
+                   # Column('comp_id', String(255)),
+                   Column('player_choice', Integer),
+                   Column('computer_choice', Integer)
+                   )
 
 engine = create_engine("mysql+mysqlconnector://root:root@localhost/RPS", echo=True)
 meta.create_all(engine)
 
 conn = engine.connect()
 
-add_author_query = authors.insert().values(name='Lutz')
-conn.execute(add_author_query)
+add_game_data = statistics.insert().values(name, player_chose, computer_chose)
+conn.execute(add_game_data)
 
-add_book_query = books.insert().values(title='learn Py',
-                                       id_author=1,
-                                       genre='Education',
-                                       price=1299)
-conn.execute(add_book_query)
-add_book_query_sec = books.insert().values(title='Clear Py',
-                                           id_author=1,
-                                           genre='Education',
-                                           price=956)
-conn.execute(add_book_query_sec)
+# add_book_query = books.insert().values(title='learn Py',
+#                                        id_author=1,
+#                                        genre='Education',
+#                                        price=1299)
+# conn.execute(add_book_query)
+# add_book_query_sec = books.insert().values(title='Clear Py',
+#                                            id_author=1,
+#                                            genre='Education',
+#                                            price=956)
+# conn.execute(add_book_query_sec)
